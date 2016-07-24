e1e92098f5c8a1de08bdb12f7a8a6342e3198f7a
Shail Choksi <choksishail@gmail.com>
2016-07-24 16:04:11 -0400

    Testing Scale Controller

---
+++ Assets/Scripts/ScaleController.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScaleContrller : MonoBehaviour {
+
+	private SteamVR_TrackedObject trackedObj;
+	private SteamVR_Controller.Device controller;
+	//private Valve.VR.EVRButtonId triggerButton = Valve.VR.EVRButtonId.k_EButton_SteamVR_Trigger;
+
+	private GameObject pickup;
+	// Use this for initialization
+	void Start () {
+		trackedObj = GetComponent<SteamVR_TrackedObject>();
+		controller = SteamVR_Controller.Input((int)trackedObj.index);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Debug.Log("In update");
+		SphereCollider s = gameObject.GetComponent<SphereCollider>();
+		if(s != null){
+			Debug.Log("Sphere object found");
+			if(controller.GetHairTriggerDown()){
+				Debug.Log("Trigger Pressed");
+				s.radius += 1;
+				//Transform selected Object
+			}
+		}
+
+		return;
+	}
+}
