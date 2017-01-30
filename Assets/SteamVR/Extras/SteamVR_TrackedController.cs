d5b943c02fa2a7a46aec6379f35534678d560160
Shail Choksi <choksishail@gmail.com>
2017-01-30 16:03:06 -0500

    Updated SteamVR plugin

--- Assets/SteamVR/Extras/SteamVR_TrackedController.cs
+++ Assets/SteamVR/Extras/SteamVR_TrackedController.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿//======= Copyright (c) Valve Corporation, All rights reserved. ===============
+using UnityEngine;
 using Valve.VR;
 
 public struct ClickedEventArgs
@@ -130,7 +131,7 @@ public class SteamVR_TrackedController : MonoBehaviour
     void Update()
     {
 		var system = OpenVR.System;
-		if (system != null && system.GetControllerState(controllerIndex, ref controllerState))
+		if (system != null && system.GetControllerState(controllerIndex, ref controllerState, (uint)System.Runtime.InteropServices.Marshal.SizeOf(typeof(VRControllerState_t))))
 		{
 			ulong trigger = controllerState.ulButtonPressed & (1UL << ((int)EVRButtonId.k_EButton_SteamVR_Trigger));
             if (trigger > 0L && !triggerPressed)
