544010d862fa9fd647f5fd80d23f966ecaee040f
Matthew Tse <mtse9779@gmail.com>
2017-02-05 22:35:19 -0500

    Code compiles!

--- Assets/Scripts/GroupUtil.cs
+++ Assets/Scripts/GroupUtil.cs
@@ -1,41 +1,46 @@
 using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
+using System;
 
 public class GroupUtil : MonoBehaviour {
-  public GameObject mergeObjects(List<GameObject> meshObjectList){
-    // List<GameObject> meshObjectList;
-    // meshObjectList.Add(obj1);
-    // meshObjectList.Add(obj2);
-    GameObject newObj;
-    CombineInstance[] combine = new CombineInstance[meshObjectList.Count];
-    int i = 0;
-    while (i < meshObjectList.Count) {
-      MeshFilter meshFilter = meshObjectList[i].gameObject.GetComponent<MeshFilter>();
-      combine[i].mesh = meshFilter.sharedMesh;
-      combine[i].transform = meshFilter.transform.localToWorldMatrix;
-      i++;
-    }
-    Mesh combinedMesh= new Mesh();
-    combinedMesh.CombineMeshes(combine);
-    newObj.GetComponent<MeshFilter>().mesh = combinedMesh;
-    //cleanup old object
-    foreach (GameObject obj in meshObjectList){
-      Destroy(obj);
-    }
-    return newObj;
+	public GameObject mergeObjects(GameObject obj1, GameObject obj2){
+		List<GameObject> meshObjectList = new List<GameObject>();
+	    meshObjectList.Add(obj1);
+	    meshObjectList.Add(obj2);
+	    GameObject newObj = new GameObject();
+	    CombineInstance[] combine = new CombineInstance[meshObjectList.Count];
+	    int i = 0;
+	    while (i < meshObjectList.Count) {
+	      MeshFilter meshFilter = meshObjectList[i].gameObject.GetComponent<MeshFilter>();
+	      combine[i].mesh = meshFilter.sharedMesh;
+	      combine[i].transform = meshFilter.transform.localToWorldMatrix;
+	      i++;
+	    }
+	    Mesh combinedMesh= new Mesh();
+	    combinedMesh.CombineMeshes(combine);
+	    newObj.GetComponent<MeshFilter>().mesh = combinedMesh;
+	    //cleanup old object
+	    foreach (GameObject obj in meshObjectList){
+	      Destroy(obj);
+	    }
+	    return newObj;
   }
 
-  public GameObject mergeGroups(List<GameObject> objects, List<Collision> allCollisions, List<GameObject> collisionObjects){
-    GameObject newObj;
-    List<GameObject> allChild = extractAllObjects(objects);
-    HashSet collisionSet;
-    for (int i = 0; i < allCollision.Count; i++) {
-      collisionSet.Add(Tuple<int,int>(allCollision[i].gameObject.GetInstanceID, collisionObjects[i].GetInstanceID));
-    }
-    for (int i = 0; i < allChild.Count; i++) {
-      for (int j = i+1; i < allChild.Count; j++) {
-        if (collisionSet.Contains(Tuple<int,int>(allChild[i].GetInstanceID,allChild[j].GetInstanceID))) {
-          mergeObjects(allChild[i],allChild[j])
+  public GameObject mergeGroups(List<GameObject> objects, HashSet<List<string>> allCollisions){
+    GameObject newObj = new GameObject();
+    // List<GameObject> allChild = extractAllObjects(objects);
+    // HashSet<List<int>> collisionSet = HashSet<List<int>>();
+		// for (int i = 0; i < allCollisions.Count; i++) {
+		// 	collisionSet.Add(Tuple.Create<int>(allCollisions[i].gameObject.GetInstanceID(), collisionObjects[i].GetInstanceID()));
+    // }
+    for (int i = 0; i < objects.Count; i++) {
+      for (int j = i+1; i < objects.Count; j++) {
+        List<string> pair = new List<string>();
+        pair.Add(objects[i].name);
+        pair.Add(objects[j].name);
+			if (allCollisions.Contains(pair)) {
+				mergeObjects (objects[i], objects[j]);
         }
       }
     }
@@ -43,38 +48,49 @@ public class GroupUtil : MonoBehaviour {
   }
 
   public GameObject groupObjects(List<GameObject> objects){
-    GameObject newObj;
-    List<GameObject> allChild = extractAllObjects(objects);
-    foreach (GameObject obj in allChild){
-	    child.transform.parent = newObj.transform;
+		GameObject newObj = new GameObject();
+    // List<GameObject> allChild = extractAllObjects(objects);
+    foreach (GameObject obj in objects){
+	    obj.transform.parent = newObj.transform;
     }
     return newObj;
   }
 
-  public int unGroupObject(int parent, int child){
-    GameObject newObj;
-    List<GameObject> allChild = extractAllObjects(objects);
-    foreach (GameObject obj in allChild){
+  public GameObject unGroupObject(List<GameObject> parent, GameObject child){
+    GameObject newObj = new GameObject();
+    // List<GameObject> allChild = extractAllObjects(parent);
+    foreach (GameObject obj in parent){
       if (obj != child){
         obj.transform.parent = newObj.transform;
       }
     }
     return newObj;
   }
+  //
+  // private void isColliding(GameObject obj1, GameObject obj2){
+  //   bool collides = false;
+  //
+  //   return collides;
+  // }
 
-  private void isColliding(GameObject obj1, GameObject obj2){
-    bool collides = false;
-
-    return collides;
-  }
-
-  private List<GameObject> extractAllObjects(List<GameObject> parents){
-    List<GameObject> allChild;
-    foreach (GameObject obj in objects){
+  public List<GameObject> extractAllObjects(List<GameObject> parents){
+    List<GameObject> allChild = new List<GameObject>();
+    foreach (GameObject obj in parents){
       foreach(Transform child in obj.transform){
-		    allChild.Add(child)
+				// allChild.Add(findObjByID(child.getInsanceID()));
+        allChild.Add(child.gameObject);
       }
     }
     return allChild;
+  }
+
+  public GameObject findObjByID(int id){
+    GameObject[] all = GameObject.FindGameObjectsWithTag("Untagged");
+    for (int i = 0; i < all.Length; i++){
+      if (all[i].GetInstanceID() == id){
+        return all[i];
+      }
+    }
+    return null;
   }
 }
