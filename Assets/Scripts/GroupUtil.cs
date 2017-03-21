478f0c688e40869fc9e0cef20a612fcdc389c666
Matthew Tse <mtse9779@gmail.com>
2017-03-21 02:54:00 -0400

    more bug squashing

--- Assets/Scripts/GroupUtil.cs
+++ Assets/Scripts/GroupUtil.cs
@@ -31,9 +31,9 @@ public class GroupUtil : MonoBehaviour {
         // connect texture to material of GameObject this script is attached to
         // GetComponent<Renderer>().material.mainTexture = texture;
         newObj.GetComponent<Renderer>().material.mainTexture = texture;
-        MeshCollider combinedMeshCollider = newObj.AddComponent<MeshCollider>();
-        combinedMeshCollider.sharedMesh = combinedMesh;
-
+        // MeshCollider combinedMeshCollider = newObj.AddComponent<MeshCollider>();
+        // combinedMeshCollider.sharedMesh = combinedMesh;
+        newObj.AddComponent<BoxCollider>();
         Debug.Log(newObj.name);
         return newObj;
     }
@@ -242,12 +242,12 @@ public class GroupUtil : MonoBehaviour {
         Vector3 min = new Vector3 (float.MaxValue, float.MaxValue, float.MaxValue);
         Vector3 max = new Vector3 (float.MinValue, float.MinValue, float.MinValue);
         for (int i = 0; i < oldVertices.Length; i++) {
-        if (oldVertices[i].x < min.x) min.x = oldVertices[i].x;
-        if (oldVertices[i].y < min.y) min.y = oldVertices[i].y;
-        if (oldVertices[i].z < min.z) min.z = oldVertices[i].z;
-        if (oldVertices[i].x > max.x) max.x = oldVertices[i].x;
-        if (oldVertices[i].y > max.y) max.y = oldVertices[i].y;
-        if (oldVertices[i].z > max.z) max.z = oldVertices[i].z;
+            if (oldVertices[i].x < min.x) min.x = oldVertices[i].x;
+            if (oldVertices[i].y < min.y) min.y = oldVertices[i].y;
+            if (oldVertices[i].z < min.z) min.z = oldVertices[i].z;
+            if (oldVertices[i].x > max.x) max.x = oldVertices[i].x;
+            if (oldVertices[i].y > max.y) max.y = oldVertices[i].y;
+            if (oldVertices[i].z > max.z) max.z = oldVertices[i].z;
         }
     
         // Make cubic buckets, each with dimensions "bucketStep"
@@ -258,42 +258,42 @@ public class GroupUtil : MonoBehaviour {
     
         // Make new vertices
         for (int i = 0; i < oldVertices.Length; i++) {
-        // Determine which bucket it belongs to
-        int x = Mathf.FloorToInt ((oldVertices[i].x - min.x) / bucketStep);
-        int y = Mathf.FloorToInt ((oldVertices[i].y - min.y) / bucketStep);
-        int z = Mathf.FloorToInt ((oldVertices[i].z - min.z) / bucketStep);
-    
-        // Check to see if it's already been added
-        if (buckets[x, y, z] == null)
-            buckets[x, y, z] = new List<int> (); // Make buckets lazily
-    
-        for (int j = 0; j < buckets[x, y, z].Count; j++) {
-            Vector3 to = newVertices[buckets[x, y, z][j]] - oldVertices[i];
-            if (Vector3.SqrMagnitude (to) < threshold) {
-            old2new[i] = buckets[x, y, z][j];
-            goto skip; // Skip to next old vertex if this one is already there
+            // Determine which bucket it belongs to
+            int x = Mathf.FloorToInt ((oldVertices[i].x - min.x) / bucketStep);
+            int y = Mathf.FloorToInt ((oldVertices[i].y - min.y) / bucketStep);
+            int z = Mathf.FloorToInt ((oldVertices[i].z - min.z) / bucketStep);
+        
+            // Check to see if it's already been added
+            if (buckets[x, y, z] == null)
+                buckets[x, y, z] = new List<int> (); // Make buckets lazily
+        
+            for (int j = 0; j < buckets[x, y, z].Count; j++) {
+                Vector3 to = newVertices[buckets[x, y, z][j]] - oldVertices[i];
+                if (Vector3.SqrMagnitude (to) < threshold) {
+                old2new[i] = buckets[x, y, z][j];
+                goto skip; // Skip to next old vertex if this one is already there
+                }
             }
-        }
     
         // Add new vertex
-        newVertices[newSize] = oldVertices[i];
-        buckets[x, y, z].Add (newSize);
-        old2new[i] = newSize;
-        newSize++;
-    
-        skip:;
+            newVertices[newSize] = oldVertices[i];
+            buckets[x, y, z].Add (newSize);
+            old2new[i] = newSize;
+            newSize++;
+        
+            skip:;
         }
     
         // Make new triangles
         int[] oldTris = mesh.triangles;
         int[] newTris = new int[oldTris.Length];
         for (int i = 0; i < oldTris.Length; i++) {
-        newTris[i] = old2new[oldTris[i]];
+            newTris[i] = old2new[oldTris[i]];
         }
         
         Vector3[] finalVertices = new Vector3[newSize];
         for (int i = 0; i < newSize; i++)
-        finalVertices[i] = newVertices[i];
+            finalVertices[i] = newVertices[i];
     
         mesh.Clear();
         mesh.vertices = finalVertices;
